ffae8121060544c8a8621caff67b3f02b7f877d6
Aziz Fikri <afikrim10@gmail.com>
2022-04-09 14:01:49 +0700

    add husky and lintstaged

---
+++ .lintstagedrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  'src/(cmd|internal|pkg)/**/*.ts?(x)': [
+    (filenames) => 'npx eslint --ext ts --fix --quiet ' + filenames.join(' '),
+    (filenames) => 'npx prettier --write ' + filenames.join(' '),
+    () => 'npm run build -- --noEmit',
+  ],
+};
